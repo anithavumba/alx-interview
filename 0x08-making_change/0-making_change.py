4d5dbe1cfe942aadd5bffd9f7f187c622b78debf
anithavumba <anithavumba@gmail.com>
2023-08-22 18:40:48 +0000

    interview

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -3,14 +3,14 @@
 def makeChange(coins, total):
     if total <= 0:
         return 0
-    
-    # Create an array to store the minimum number of coins needed for each total
+
+    # Create an array to store minimum coin count for each total
     dp = [float('inf')] * (total + 1)
     dp[0] = 0
-    
+
     for i in range(1, total + 1):
         for coin in coins:
             if i - coin >= 0:
                 dp[i] = min(dp[i], dp[i - coin] + 1)
-    
+
     return dp[total] if dp[total] != float('inf') else -1
